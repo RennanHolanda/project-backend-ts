9b013d0404743c06811506a7775e327e7fd4ee31
Bruno Rennan <bruno_rennan@hotmail.com>
2023-01-23 15:04:35 -0300

    criação de logs e middlewares

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,22 @@
 import { Router, Request, Response } from 'express';
-import { createMovie } from './controllers/movieController';
+import { createMovie,
+         findMovieById,
+         getAllMovie,
+         removeMovie,
+         updateMovie } from './controllers/movieController';
 
 //validation
 
 import { validate } from './middleware/handleValidation';
+import { movieCreateValidation } from './middleware/movieValidation';
 
 const router = Router();
 
 export default router.get("/test", (req: Request, res: Response) => {
     res.status(200).send("API working")
 })
-.post("/movie", validate, createMovie)
+.post("/movie", movieCreateValidation(), validate, createMovie)
+.get("/movie/:id", findMovieById)
+.get("/movie", getAllMovie)
+.delete("/movie/:id", removeMovie)
+.patch("/movie/:id",movieCreateValidation(), validate, updateMovie)
